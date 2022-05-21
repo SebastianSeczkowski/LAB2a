a275789fd87e15a42fbcee2a3e4256f99182d04b
SebastianSeczkowski <37137@studentwsei.pl>
2022-05-21 23:52:50 +0200

    zawartosc pliku gusinfographic.xml

--- test/TestFileRead.java
+++ test/TestFileRead.java
@@ -5,16 +5,17 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TestFileRead {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException{
         try {
             String contents = new String(Files.readAllBytes(Paths.get("gusInfoGraphic.xml")));
             System.out.println("ZAWARTOSC PLIKU gusInfoGraphic.xml");
             System.out.print(contents);
-        }
+       }
         catch (IOException e) {
-            System.out.println("Błąd wczytywania pliku gusInfoGraphic.xml => "+ e.getLocalizedMessage());
-            e.printStackTrace();
+      System.out.println("Błąd wczytywania pliku gusInfoGraphic.xml => "+ e.getLocalizedMessage());
+      e.printStackTrace();
         }
     }
 
 }
+
