6cb97b0aca412b85b3e8f92e93b67429689af84a
SebastianSeczkowski <37137@studentwsei.pl>
2022-05-21 19:44:05 +0200

    utworzenie statycznej metody main

--- test/TestFileRead.java
+++ test/TestFileRead.java
@@ -1,6 +1,20 @@
 package pl.lublin.wsei.java.cwiczenia.test;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class TestFileRead {
-    
-    
+    public static void main(String[] args) {
+        try {
+            String contents = new String(Files.readAllBytes(Paths.get(first:"gusInfoGraphic.xml")));
+            System.out.println("ZAWARTOSC PLIKU gusInfoGraphic.xml");
+            System.out.print(contents);
+        }
+        catch (IOException e) {
+            System.out.println("Błąd wczytywania pliku gusInfoGraphic.xml => "+ e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+    }
+
 }
