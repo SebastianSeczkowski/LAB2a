c99b946d927e46ea63b2822afc4db1b6b3e814e2
SebastianSeczkowski <37137@studentwsei.pl>
2022-05-22 00:26:13 +0200

    kod wychwytujący atrybut tytul

--- TestRegEx.java
+++ TestRegEx.java
@@ -1,5 +1,7 @@
 package pl.lublin.wsei.java.cwiczenia.test;
 
+import java.util.regex.Pattern;
+
 public class TestRegEx {
     public static void main(String[] args) {
         String exItem = "\t\t<item>\n" +
@@ -9,6 +11,13 @@ public class TestRegEx {
                 "\t\t\t<media:content url=\"https://stat.gov.pl/files/gfx/portalinformacyjny/pl/defaultaktualnosci/5866/117/1/1/spoleczenstwoinformacyjne_infografika_pl.png\" type=\"image/png\" width=\"1876\" height=\"2855\">\n" +
                 "\t\t\t<media:thumbnail url=\"https://stat.gov.pl//gfx/portalinformacyjny/_thumbs/pl/defaultaktualnosci/5866/117/1/1/spoleczenstwoinformacyjne_infografika_pl,k1uUwl-caFOE6tCTiHtf.png\" />\n" +
                 "\t\t\t<description><![CDATA[<div><img src=\"https://stat.gov.pl//gfx/portalinformacyjny/_thumbs/pl/defaultaktualnosci/5866/117/1/1/spoleczenstwoinformacyjne_infografika_pl,k1uUwl-caFOE6tCTiHtf.png\" alt=\"\" width=\"280\" height=\"212\"/></div>]]></description> \n" +
-                "\t\t\t</item>\n" +
+                "\t\t\t</item>" +
+                
+                Pattenr pat = Pattern.compile("<title><!\\[CDATA\\[(.*)\\]\\]");
+        Matcher m = pat.matcher(exItem);
+        if (m.find())
+            System.out.println("Znaleziono tytuł: "+m.group(1));
+        else
+            System.out.println("Nie znaleziono tytułu ...");
     }
 }
