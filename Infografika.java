46fa129e6480b3310a62289706c9d72c9aba05d9
SebastianSeczkowski <37137@studentwsei.pl>
2022-05-22 00:50:41 +0200

    konstruktor klasy Infografika

--- Infografika.java
+++ Infografika.java
@@ -1,4 +1,24 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-public class Infografika {
+public Infografika(String tekst){
+    Pattern pat = Pattern.compile("<title><!\\]CDATA]\\[(.*)\\]\\]");
+    Matcher m = pat.matcher(tekst);
+    if (m.find())
+        tytul = m.group(1);
+    else
+        tytul = "";
+    
+    pat = Pattern.compile("<link>(.*)</link>");
+    m = pat.matcher(tekst);
+    if (m.find())
+        adresStrony = m.group(1);
+    else
+        adresStrony = "";
+    
+    pat = Pattern.compile("width=\"(.*)\" height");
+    m = pat.matcher (tekst);
+    if (m.find())
+        szerokosc = Integer.parseInt(m.group(1));
+    else
+        szerokosc = 0;
 }
